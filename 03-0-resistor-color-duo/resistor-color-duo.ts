72c6ae32c5dc01ace66f2ab5f518dfcaf1a79453
kkata <14849535+kkata@users.noreply.github.com>
2021-08-25 23:10:13 +0900

    modified 03

--- 03-0-resistor-color-duo/resistor-color-duo.ts
+++ 03-0-resistor-color-duo/resistor-color-duo.ts
@@ -1,8 +1,26 @@
-export class ResistorColor {
-  private colors: string[]
+const Colors = [
+  "black",
+  "brown",
+  "red",
+  "orange",
+  "yellow",
+  "green",
+  "blue",
+  "violet",
+  "grey",
+  "white",
+] as const;
 
-  constructor(colors: string[]) {
-    this.colors = colors
+type Color = typeof Colors[number];
+
+export class ResistorColor {
+  constructor(private readonly colors: Color[]) {
+    if (colors.length < 2) {
+      throw new Error("At least two colors need to be present");
+    }
+    this.colors = colors;
   }
-  value = (): number => 0
+
+  value = (): number =>
+    Colors.indexOf(this.colors[0]) * 10 + Colors.indexOf(this.colors[1]);
 }
