4b25f74adbbd8b9d36931413efc88c40719f915e
kkata <14849535+kkata@users.noreply.github.com>
2021-08-30 23:12:37 +0900

    04

--- 04-0-leap/leap.ts
+++ 04-0-leap/leap.ts
@@ -1,11 +1,5 @@
 function isLeapYear(year: number) {
-  if (year % 4 === 0) {
-    if (year % 100) {
-      if (year % 400 !== 0) return false;
-    }
-    return false;
-  }
-  return false;
+  return !!((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0);
 }
 
 export default isLeapYear;
